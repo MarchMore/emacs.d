937d93ad6143b61d46b9a8489d1b2f0d708023ad
yangning <yangning1123@gmail.com>
2014-02-21 20:27:30 +0800

    *** empty log message ***

---
+++ init.el
@@ -0,0 +1,8 @@
+;;; This file bootstraps the configuration, which is divided into
+;;; a number of other files.
+
+(let ((minver 23))
+  (unless (>= emacs-major-version minver)
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+
+(add-to-list 'load-path (expand-file-name "site-lisp" user-emacs-directory))
