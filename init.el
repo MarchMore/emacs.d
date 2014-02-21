fe3a32fa3f45b6bc97e67ee81beed0bbb9a49711
yangning <yangning1123@gmail.com>
2014-02-21 20:30:02 +0800

    *** empty log message ***

--- init.el
+++ init.el
@@ -6,3 +6,7 @@
     (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
 
 (add-to-list 'load-path (expand-file-name "site-lisp" user-emacs-directory))
+
+
+(require 'init-elpa)
+(require 'init-misc)
