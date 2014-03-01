b551b0590cd0c7f3337de5143be2217883aa3fca
yangning <yangning1123@gmail.com>
2014-03-01 13:40:07 +0800

    显示当前光标所在位置c/c++汇编代码

--- site-lisp/init-cc.el
+++ site-lisp/init-cc.el
@@ -1,3 +1,5 @@
+(require 'cc-mode)
+
 ;; Stroustrup风格
 (add-hook 'c-mode-hook
 	  '(lambda ()
@@ -10,6 +12,9 @@
 (global-set-key [f6] 'gdb)              ;f6调试
 (global-set-key [f7] 'gdb-many-windows) ;f7启用gdb多窗口功能
 
+;; 汇编光标所在位置代码
+(require 'disaster)
+(define-key c-mode-base-map (kbd "C-c d") 'disaster)
 
 (provide 'init-cc)
  
