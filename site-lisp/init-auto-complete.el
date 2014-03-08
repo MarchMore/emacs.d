f4b22c91db303a680da87ac5d173c99cfa86091d
yangning <yangning1123@gmail.com>
2014-03-08 16:20:50 +0800

    auto-complete-clang-async配置

--- site-lisp/init-auto-complete.el
+++ site-lisp/init-auto-complete.el
@@ -9,7 +9,21 @@
 
 
 ;;;; auto-complete
-(require 'auto-complete)
-(global-auto-complete-mode t)
+(require 'auto-complete-config)
+(ac-config-default)             ; 简单的补全
+
+;; c++代码使用clang-async分析补全
+(require 'auto-complete-clang-async)
+(defun ac-cc-mode-setup ()
+  (setq ac-clang-complete-executable "~/.emacs.d/clang-complete")
+  (setq ac-sources '(ac-source-clang-async))
+  (ac-clang-launch-completion-process)
+)
+(defun my-ac-config ()
+  (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
+  (global-auto-complete-mode t))
+(my-ac-config)
+
 
 (provide 'init-auto-complete)
